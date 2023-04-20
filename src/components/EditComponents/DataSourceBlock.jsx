b5597f2cc596cfbbe38f19d34a0eaf68198b3ac5
yichien <kevinyichienlee@gmail.com>
2023-04-21 00:50:55 +0800

    0420

---
+++ src/components/EditComponents/DataSourceBlock.jsx
@@ -0,0 +1,166 @@
+import { Box, Button, TextField } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchErrorShowBorder,
+  updateData,
+  updateDataSourceWithURL,
+} from "../../store";
+
+import "ace-builds/src-noconflict/mode-json";
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/ext-language_tools";
+import { useRef, useState } from "react";
+import axios from "axios";
+import InspectDrawer from "../InspectDrawer";
+import VariableAccordion from "./DataSourceComponent/VariableAccordion";
+import RealTimeChart from "../testComponents/RealTimeChart";
+
+const DataSourceBlock = ({ panelID }) => {
+  const dispatch = useDispatch();
+  const textRef = useRef("");
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const { datasource_url, fetchError, fetchErrorMessage } = useSelector(
+    (state) => {
+      const panelArray = state.widget.widgetArray;
+      const targetPanel = panelArray.filter((panel) => panel.i === panelID);
+      return {
+        datasource_url: targetPanel[0]?.data?.datasource_url,
+        fetchError: targetPanel[0]?.fetchError,
+        fetchErrorMessage: targetPanel[0]?.fetchErrorMessage,
+      };
+    }
+  );
+
+  const [textValue, setTextValue] = useState(datasource_url || "");
+
+  let variablesArray = useSelector((state) => {
+    return state.variable.variableArray;
+  });
+
+  const fetchURl = async (variablesArray, currentText) => {
+    try {
+      // Define default values for each variable
+      let defaultValues = {};
+
+      variablesArray.forEach(({ variableName, defaultValue }) => {
+        defaultValues[variableName] = defaultValue;
+      });
+      console.log(
+        "🚀 ~ file: DataSourceBlock.jsx:41 ~ fetchURl ~ defaultValues:",
+        defaultValues
+      );
+      console.log("before Regex ARRAY: ", variablesArray);
+
+      console.log("before regex:", currentText);
+
+      currentText = currentText.replace(/\@(\w+)/g, (match, variableName) => {
+        const variableValue = defaultValues[variableName];
+        return variableValue !== undefined ? variableValue : match;
+      });
+      console.log("after regex:", currentText);
+      if (currentText === "http://localhost:5001/updates") return;
+      if (currentText === "http://localhost:5001/updates/v2") return;
+      const response = await axios.get(currentText);
+      const data = response.data;
+      dispatch(updateData({ data, panelID }));
+      const res = false;
+      const id = panelID;
+      const message = "";
+      dispatch(fetchErrorShowBorder({ id, res, message }));
+    } catch (error) {
+      const res = true;
+      const id = panelID;
+      const message = error.message;
+      dispatch(fetchErrorShowBorder({ id, res, message }));
+    }
+  };
+
+  const handleSetURL = (datasourceName, datasource_url, panelID) => {
+    dispatch(
+      updateDataSourceWithURL({ datasourceName, datasource_url, panelID })
+    );
+  };
+
+  //border: "1px solid black"
+  return (
+    <Box sx={{ margin: "5px" }}>
+      <Box component="div" sx={{}} overflow="hidden">
+        DataSourceBlock
+      </Box>
+
+      <Box display="flex" alignItems="center">
+        <Button
+          disableRipple
+          disableFocusRipple
+          disableElevation
+          sx={{
+            color: "#5B9AFF",
+            backgroundColor: "#181B1F",
+            width: "10%",
+            marginRight: "1rem",
+            "&:hover": { backgroundColor: "#181B1F" },
+          }}
+          variant="contained"
+        >
+          URL
+        </Button>
+        <TextField
+          error={fetchError ? true : false}
+          sx={{ backgroundColor: "#141414" }}
+          fullWidth
+          inputRef={textRef}
+          hiddenLabel
+          id="filled-hidden-label-small"
+          value={textValue}
+          variant="filled"
+          helperText={fetchErrorMessage ? `${fetchErrorMessage}` : ""}
+          size="small"
+          onChange={(e) => {
+            setTextValue(e.target.value);
+            const url = e.target.value;
+            handleSetURL("link", url, panelID);
+            if (url !== "http://localhost:5001/updates")
+              fetchURl(variablesArray, textRef.current.value);
+          }}
+        />
+        <Button
+          variant="contained"
+          style={{
+            textTransform: "unset",
+            marginLeft: "1rem",
+          }}
+          sx={{
+            fontSize: { sm: "10px", lg: "14px" },
+            padding: { sm: "0", lg: "0.5rem" },
+            width: { sm: "5%", lg: "15%" },
+          }}
+          onClick={() => setDrawerOpen(true)}
+        >
+          Query inspector
+        </Button>
+      </Box>
+      {variablesArray.length ? (
+        <VariableAccordion
+          fetchURl={fetchURl}
+          panelID={panelID}
+          setTextValue={setTextValue}
+          handleSetURL={handleSetURL}
+          textRef={textRef}
+        />
+      ) : (
+        ""
+      )}
+
+      {/* <RealTimeChart /> */}
+
+      <InspectDrawer
+        panelID={panelID}
+        drawerOpen={drawerOpen}
+        setDrawerOpen={setDrawerOpen}
+      />
+    </Box>
+  );
+};
+
+export default DataSourceBlock;
---
+++ src/components/testComponents/RealTimeChart.jsx
@@ -0,0 +1,147 @@
+import React, { useState, useEffect, useCallback } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  LineChart,
+  Line,
+  ResponsiveContainer,
+  Brush,
+} from "recharts";
+import { fetchEventSource } from "@microsoft/fetch-event-source";
+import { useDispatch, useSelector } from "react-redux";
+import { updateData } from "../../store";
+
+const serverBaseURL = "http://localhost:5001";
+
+function formatTime(timestamp) {
+  const date = new Date(timestamp);
+  return `${date.toLocaleTimeString("en-US", { hour12: false })}`;
+}
+
+export default function RealTimeChart({ panelID }) {
+  let { serverURL, dataDetail } = useSelector((state) => {
+    const panelArray = state.widget.widgetArray;
+    const targetPanel = panelArray.filter((panel) => panel.i === panelID);
+    return {
+      serverURL: targetPanel[0]?.data?.datasource_url,
+      dataDetail: targetPanel[0]?.data?.dataDetail,
+    };
+  });
+  console.log(
+    "file: RealTimeChart.jsx:34 ~ let{serverURL}=useSelector ~ serverURL:",
+    serverURL
+  );
+  const dispatch = useDispatch();
+
+  const [domain, setDomain] = useState([null, null]);
+  const [eventSource, setEventSource] = useState(null);
+  const [data, setData] = useState([]);
+  // Dispatch data after it has been initialized
+  // if (data.length > 0) {
+  //   dispatch(updateData({ data, panelID }));
+  // }
+
+  useEffect(() => {
+    let es = null;
+    const controller = new AbortController();
+    const fetchData = async () => {
+      if (serverURL) {
+        es = await fetchEventSource(serverURL, {
+          signal: controller.signal,
+          onmessage(ev) {
+            const newData = JSON.parse(ev.data);
+            console.log(
+              "🚀 ~ file: RealTimeChart.jsx:58 ~ onmessage ~ newData:",
+              newData
+            );
+            setData((prevData) => [...prevData, newData]); // update newDataArray
+          },
+          onclose() {
+            console.log("Connection Closed by the Server");
+          },
+          onerror(err) {
+            console.log("There was an error from the Server!", err);
+          },
+        });
+
+        setEventSource(es);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      if (es) {
+        es.close();
+      }
+      controller.abort();
+    };
+  }, [serverURL]);
+
+  // update Redux store with newDataArray when it changes
+  useEffect(() => {
+    if (data.length > 0) {
+      dispatch(updateData({ data, panelID }));
+    }
+  }, [data, dispatch, panelID]);
+
+  useEffect(() => {
+    setData([]); // clear data array when serverURL changes
+  }, [serverURL]);
+
+  // calculate domain of XAxis based on current time
+  const now = new Date();
+  const defaultDomain = [now.getTime() - 60000, now.getTime()];
+
+  // filter data to show only the past 10 seconds
+  const filteredData = data.filter((d) => d.timestamp > now.getTime() - 60000);
+  // handle chart domain change on brush change event
+  const handleBrushChange = (event) => {
+    if (event.startIndex === 0 && event.endIndex === filteredData.length - 1) {
+      setDomain(null);
+    } else {
+      setDomain([
+        filteredData[event.startIndex].timestamp,
+        filteredData[event.endIndex].timestamp,
+      ]);
+    }
+  };
+  // calculate the YAxis domain to center the line chart
+  // const yValues = filteredData.map((d) => d.value);
+  // const minValue = Math.min(...yValues);
+  // const maxValue = Math.max(...yValues);
+  // const range = maxValue - minValue;
+  // const middleValue = minValue + range / 2;
+  // const yDomain = [middleValue - range / 2, middleValue + range / 2];
+
+  // render chart using `data`
+  return (
+    <ResponsiveContainer>
+      <LineChart data={filteredData}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis
+          dataKey="timestamp"
+          domain={domain || defaultDomain}
+          tickFormatter={formatTime}
+          type="number"
+        />
+        <YAxis />
+        <Tooltip labelFormatter={formatTime} />
+        <Legend />
+        <Line dataKey="value" stroke="#8884d8" />
+        {/* <Brush
+            stroke="#8884d8"
+            dataKey="timestamp"
+            startIndex={filteredData.length - 1000}
+            endIndex={filteredData.length - 1}
+            onChange={handleBrushChange}
+          /> */}
+      </LineChart>
+    </ResponsiveContainer>
+  );
+}
